736c7d90d35206918a67752c0d852c068b916928
tamim-jr <internationalinf24@gmail.com>
2023-09-03 21:29:50 +0600

    add login code in auth for collect login data form postman

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,19 @@
 const router = require('express').Router();
 const User = require("../models/User");
+const bcrypt = require('bcrypt');
 
 //Register
 router.post('/register', async (req, res) =>{
     try{
+        //for password hash
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+        //for get data from user
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPass,
         });
 
         const user = await newUser.save();
@@ -16,5 +22,20 @@ router.post('/register', async (req, res) =>{
         res.status(500).json(err);
     }
 });
+
+//login
+ router.post('/login', async (req, res) =>{
+    try{
+        const user = await User.findOne({username: req.body.username})
+        !user && res.status(400).json("Wrong Credentials!")
+
+        const validated = await bcrypt.compare(req.body.password, user.password)
+        ! validated && res.status(400).json("Wrong Credentials!")
+
+        res.status(200).json(user);
+    }catch (err){
+        res.status(500).json(err);
+    }
+ });
 
 module.exports = router;
